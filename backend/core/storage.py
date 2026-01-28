1571b786bcaf5a7899455b54339faf7dd465374c
Kush05Bhardwaj <kush2012bhardwaj@gmail.com>
2026-01-28 23:24:37 +0530

    file_handling setup

---
+++ backend/core/storage.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import path
+from .exceptions import TaskNotFoundError
+
+DATA_FILE = path.Path("tasks.json")
+
+def load_tasks():
+    if not DATA_FILE.exists():
+        return []
+    with open(DATA_FILE, "r") as f:
+        return json.load(f)
+    
+def save_tasks(tasks):
+    with open(DATA_FILE, "w") as f:
+        json.dump(tasks, f, indent=4)
+
+def find_task(task_id, tasks):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+        else:
+            raise TaskNotFoundError(f"Task with ID {task_id} not found")
